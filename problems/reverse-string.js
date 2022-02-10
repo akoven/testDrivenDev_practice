67cb616c3e60ad6680309ee8dcc249e9aa5b4f3b
Attiya <adarensburg@live.com>
2022-02-10 13:08:27 -0600

    pushing reverse-string exercise

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,10 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  let arr = string.split('')
+  let revString = arr.reverse();
+  return revString.join('');
 };
+
+// let word = 'water'
+// reverseString(word);
+
+// module.exports = reverseString;
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,9 @@
-// Your code here
+const {expect} = require('chai');
+const reverseString = require('../problems/reverse-string')
+
+describe('reverseString',function(){
+    it('should reverse the word', function(){
+        // expect.fail();
+        expect(reverseString('fun')).to.equal('nuf');
+    })
+})
