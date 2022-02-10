47adc23c702c333590982c996f7ec68044b94506
Waseemalame <waseemalame45@gmail.com>
2022-02-10 15:02:51 -0800

    person-unsolved

--- problems/person.js
+++ problems/person.js
@@ -1,5 +1,40 @@
 class Person {
-  // Your code here
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  sayHello() {
+    return `${this.name} welcome!`
+  }
+  visit(otherPerson) {
+    return `${otherPerson} visited ${this.name}`
+  }
+  switchVisit(otherPerson) {
+    this.visit(otherPerson)
+  }
+  update(obj) {
+    if (!(obj instanceof 'Object')) {
+      throw new TypeError('input is not an object')
+    } else if (obj instanceof 'Object') {
+      this.name = obj.name;
+      this.age = obj.age;
+    } else if (obj.name === undefined || obj.age === undefined) {
+      throw new TypeError('object does not have property values')
+    }
+  }
+  tryUpdate(obj) {
+    return this.update(obj);
+  }
+  static greetAll(obj) {
+    let newArr = [];
+    for (let i = 0; i < obj.length; i++) {
+      const element = obj[i];
+
+      newArr.push(sayHello(element));
+
+    }
+    return newArr;
+  }
 }
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,39 @@
-// Your code here
+const chai = require('chai')
+const expect = chai.expect
+const { Person } = require('../problems/person')
+
+
+describe('Person', function () {
+    let newPerson;
+    beforeEach(() => {
+        newPerson = new Person({ name: 'ali', age: '38' })
+        describe('sayHello', function () {
+
+            it('should return a string of the Person instance"s name and a greeting message', function () {
+                expect.fail()
+            })
+        })
+    })
+})
+        // describe('switchVisit', function () {
+        //     it('should return a string stating that this instance visited the passed-in person instance', function () {
+        //         expect.fail()
+        //     })
+        // })
+        // describe('update(obj)', function () {
+        //     it('should return a string stating that this instance visited the passed-in person instance', function () {
+        //         expect.fail()
+        //     })
+        // })
+        // describe('tryUpdate', function () {
+        //     it('should return a string stating that this instance visited the passed-in person instance', function () {
+        //         expect.fail()
+        //     })
+        // })
+        // describe('greetAll', function () {
+        //     it('should return a string stating that this instance visited the passed-in person instance', function () {
+        //         expect.fail()
+        //     })
+        // })
+
+    // })
