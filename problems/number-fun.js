bfbab3e201ad038fb0161183c5394f616a0eaac0
Attiya <adarensburg@live.com>
2022-02-10 15:00:50 -0600

    finished phase 2

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -3,9 +3,21 @@ function returnsThree() {
 }
 
 function reciprocal(n) {
+  if(n < 1 || n > 1000000){
+    throw new TypeError('Your number must be between 1 and 10^6')
+  }
   return 1/n;
 }
 
+try{
+  reciprocal(5)
+  reciprocal(50)
+  reciprocal(1000001)
+}catch(error){
+  console.error(error.name, error.message)
+}
+
+console.log(reciprocal(1000001))
 module.exports = {
   returnsThree,
   reciprocal
