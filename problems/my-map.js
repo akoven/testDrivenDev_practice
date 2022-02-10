4f2f5834a9d3b6ddf0c7e97b0ce945e429c71ca9
Attiya <adarensburg@live.com>
2022-02-10 15:58:38 -0600

    completed phase 3

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,14 +1,17 @@
+
 function myMap(inputArray, callback) {
-  // let newArr = [];
-  // for (let el of inputArray) {
-  //   newArr.push(callback(el))
-  // }
-  return inputArray.map(el => callback(el))
+  let newArr = [];
+  for (let el of inputArray) {
+    newArr.push(callback(el));
+  }
+
+  return newArr;
 }
+
 let calling = (num) => {
-  return num * 2
+  return num *= 2
 }
 
-
+console.log(myMap([1,2,3],calling))
 // console.log(myMap((myArr), calling))
 module.exports = { myMap };
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,12 +1,13 @@
-const { expect } = require('chai');
+const chai  = require('chai');
+const expect = chai.expect;
 const { myMap } = require('../problems/my-map')
 
 describe('myMap', function () {
-    it('should multiply every element by 2 and return new array', function () {
-        let myArr = [1, 2, 3]
-        let callback = function (num) {
-            num * 2
-        }
-        expect(myMap(myArr), callback()).to.equal([2, 4, 6])
+    let calling;
+    beforeEach(() => {
+        calling = (num) => num*2
+    })
+    it('should multiply every element by 2 and return new array', function (){
+        expect(myMap([1,2,3], calling)).to.eql([2, 4, 6])//we need deep equal here(.eql)
     })
 })
